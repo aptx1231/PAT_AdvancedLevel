eb8a92f86316f36ab1ebd8a34f174e4c342ccdb6
jjw <jwjiang_buaa@163.com>
2019-10-25 16:53:52 +0800

    updata_1026

--- 1026 Table Tennis (30 分).cpp
+++ 1026 Table Tennis (30 分).cpp
@@ -5,16 +5,18 @@
 #include <string>
 using namespace std;
 
+//If one cannot get a table before the closing time, their information must NOT be printed.
+//截止时间前无法得到服务的就不服务了
+
 struct node {
-    int id;
     int arriveTime;
     int startTime;
     int serveTime;
     int waitTime;
     int isVip;
     int tableId;
-    node(int id_ = 0, int ar = 0, int se = 0, int iv = 0, int st = 0, int wt = 0, int ti = -1) :
-            arriveTime(ar), startTime(st), serveTime(se), isVip(iv), tableId(ti), id(id_), waitTime(wt) {}
+    node(int ar = 0, int se = 0, int iv = 0, int st = 0, int wt = 0, int ti = -1) :
+            arriveTime(ar), startTime(st), serveTime(se), isVip(iv), tableId(ti), waitTime(wt) {}
 };
 
 struct table {
@@ -61,7 +63,7 @@ int main() {
         if (serve > 120) {   //最多2小时 即120分钟  超时需要压缩时间
             serve = 120;
         }
-        que.push_back(node(i, trans(time), serve*60, isVip));
+        que.push_back(node(trans(time), serve*60, isVip));
     }
     int m,k;
     cin >> k >> m;
@@ -71,14 +73,14 @@ int main() {
         tables[tmp].vip = 1;
     }
     sort(que.begin(), que.end(), com);
-    for (vector<node>::iterator it = que.begin(); it != que.end(); ) {
+    for (int i=0;i<que.size();) {
         int minEndTime = 21*60*60;
         int poi = 0;
-        //找最早结束的服务台 
-        for (int i=1;i<=k;i++) {
-            if (tables[i].endTime < minEndTime) {
-                minEndTime = tables[i].endTime;
-                poi = i;
+        //找最早结束的服务台
+        for (int j=1;j<=k;j++) {
+            if (tables[j].endTime < minEndTime) {
+                minEndTime = tables[j].endTime;
+                poi = j;
             }
         }
         if (minEndTime == 21*60*60) {  //所有服务器最早结束的时间都超过21点 后边的顾客就不能被服务了
@@ -87,86 +89,86 @@ int main() {
         if (tables[poi].vip) {  //最早结束的是vip桌
             //先看当前时间minEndTime，队列中有没有vip
             int flag = 1;
-            for (vector<node>::iterator it_cur = it; it_cur != que.end() && it_cur->arriveTime <= minEndTime; it_cur++) {
-                if (it_cur->isVip && it_cur->tableId == -1) {
-                    it_cur->tableId = poi;
-                    if (minEndTime < it_cur->arriveTime) {
-                        it_cur->startTime = it_cur->arriveTime;
-                        it_cur->waitTime = 0;
+            for (int l=i; l < que.size() && que[l].arriveTime <= minEndTime; l++) {
+                if (que[l].isVip && que[l].tableId == -1) {
+                    que[l].tableId = poi;
+                    if (minEndTime < que[l].arriveTime) {
+                        que[l].startTime = que[l].arriveTime;
+                        que[l].waitTime = 0;
                     }
                     else {
-                        it_cur->startTime = minEndTime;
-                        it_cur->waitTime = (minEndTime - it_cur->arriveTime);
+                        que[l].startTime = minEndTime;
+                        que[l].waitTime = (minEndTime - que[l].arriveTime);
                     }
-                    tables[poi].endTime = it_cur->startTime + it_cur->serveTime;
-                    if (it_cur->startTime < 21*60*60) {
+                    tables[poi].endTime = que[l].startTime + que[l].serveTime;
+                    if (que[l].startTime < 21*60*60) {
                         tables[poi].num++;
                     }
-                    flag = 0;  //找到了vip 此时it不能++
+                    flag = 0;  //找到了vip 此时i不能++
                     break;
                 }
             }
             if (flag) {  //没有找到vip 则选择第一个人
-                it->tableId = poi;
-                if (minEndTime < it->arriveTime) {
-                    it->startTime = it->arriveTime;
-                    it->waitTime = 0;
+                que[i].tableId = poi;
+                if (minEndTime < que[i].arriveTime) {
+                    que[i].startTime = que[i].arriveTime;
+                    que[i].waitTime = 0;
                 }
                 else {
-                    it->startTime = minEndTime;
-                    it->waitTime = (minEndTime - it->arriveTime);
+                    que[i].startTime = minEndTime;
+                    que[i].waitTime = (minEndTime - que[i].arriveTime);
                 }
-                tables[poi].endTime = it->startTime + it->serveTime;
-                if (it->startTime < 21*60*60) {
+                tables[poi].endTime = que[i].startTime + que[i].serveTime;
+                if (que[i].startTime < 21*60*60) {
                     tables[poi].num++;
                 }
-                it++;
+                i++;
             }
         }
         else {  //最早结束的不是vip桌
-            if (it->isVip) {  //第一个人是vip 应该优先选择vip桌 先去找当前时间it->arriveTime有没有vip桌
-                if (it->tableId == -1) {  //没有被分配过桌子的vip 因为vip可能在之前有vip桌空闲时 直接被服务了
+            if (que[i].isVip) {  //第一个人是vip 应该优先选择vip桌 先去找当前时间que[i].arriveTime有没有vip桌
+                if (que[i].tableId == -1) {  //没有被分配过桌子的vip 因为vip可能在之前有vip桌空闲时 直接被服务了
                     int vipTable = -1;
-                    for (int i=1;i<=k;i++) {  //结束时间要在当前人的到达时间之前
-                        if (tables[i].vip && tables[i].endTime <= it->arriveTime) {
-                            vipTable = i;
+                    for (int j=1;j<=k;j++) {  //结束时间要在当前人的到达时间之前
+                        if (tables[j].vip && tables[j].endTime <= que[i].arriveTime) {
+                            vipTable = j;
                             break;
                         }
                     }
                     if (vipTable != -1) {  //找到vip桌了
-                        it->tableId = vipTable;
+                        que[i].tableId = vipTable;
                         poi = vipTable;  //为了后边可以合在一起写
                     }
                     else {  //没有vip桌
-                        it->tableId = poi;
+                        que[i].tableId = poi;
                     }
                 }
                 else {  //队首是一个被服务过的vip 直接越过
-                    it++;
+                    i++;
                     continue;
                 }
             }
             else {  //第一个人也不是vip 直接分配
-                it->tableId = poi;
+                que[i].tableId = poi;
             }
-            if (minEndTime < it->arriveTime) {
-                it->startTime = it->arriveTime;
-                it->waitTime = 0;
+            if (minEndTime < que[i].arriveTime) {
+                que[i].startTime = que[i].arriveTime;
+                que[i].waitTime = 0;
             }
             else {
-                it->startTime = minEndTime;
-                it->waitTime = (minEndTime - it->arriveTime);
+                que[i].startTime = minEndTime;
+                que[i].waitTime = (minEndTime - que[i].arriveTime);
             }
-            tables[poi].endTime = it->startTime + it->serveTime;
-            if (it->startTime < 21*60*60) {
+            tables[poi].endTime = que[i].startTime + que[i].serveTime;
+            if (que[i].startTime < 21*60*60) {
                 tables[poi].num++;
             }
-            it++;
+            i++;
         }
     }
     sort(que.begin(), que.end(), com2);
     for (int i=0;i<que.size();i++) {
-    	//限制在8-21点之间 21点前没有执行到的人的startTime是初始值0 不能输出 
+        //限制在8-21点之间 21点前没有执行到的人的startTime是初始值0 不能输出
         if (que[i].startTime >= 8*60*60 && que[i].startTime < 21*60*60) {
             showTime(que[i].arriveTime);
             showTime(que[i].startTime);
